49bef896f0a080e61e3310a0a01b6c1c2143fe29
Katie Dawkins <katiemdawkins@gmail.com>
2022-01-12 16:04:21 -0600

    baking cookies

---
+++ main.js
@@ -0,0 +1,48 @@
+// I like to bake chocolate chip cookies.
+
+//I need to set butter out at room temp.
+//Gather other ingredients.
+// Turn oven on.
+//Mix batter.
+    //Mix dry ingredients in one bowl.
+    //Cream butter and sugar.
+    //Add eggs and vanilla.
+    //Add dry ingredients to wet ingredients.
+//Roll dough into balls and put on baking sheet.
+//Bake cookies.
+//Remove cookies from oven.
+//Eat while hot.
+
+const butterRoomTemp = () => {
+    console.log("Set butter on the counter until it comes to room temperature.")
+}
+const gatherIngredients = () => {
+    console.log("Gather all other ingredients.")
+}
+const turnOnOven = () => {
+    console.log("Turn the oven on to 350.")
+}
+const mixBatter = () => {
+    console.log("Mix dry ingredients in one bowl. Cream butter and sugar. Add eggs and vanilla. Add dry ingredients to wet ingredients.")
+}
+const rollDough = () => {
+    console.log("Roll dough into balls and put on baking sheet.")
+}
+const bake = () => {
+    console.log("Bake cookies for 9 minutes.")
+}
+const removeFromOven = () => {
+    console.log("They should be gooey when they are done")
+}
+const eat = () => {
+    console.log("Eat them while hot, perfect and gooey!")
+}
+
+butterRoomTemp()
+gatherIngredients()
+turnOnOven()
+mixBatter()
+rollDough()
+bake()
+removeFromOven()
+eat()
